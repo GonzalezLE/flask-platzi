e5d392559223321f4fb565432baba2d504884bcd
GonzalezLE <gonzalez-3216@hotmail.com>
2022-10-16 21:10:02 -0500

    correccion d error y intalando controlador de db

--- app/auth/view.py
+++ app/auth/view.py
@@ -1,18 +1,33 @@
 from typing import Dict
 
-from flask import render_template
+from flask import (
+    render_template,
+    session,
+    flash,
+    redirect,
+    url_for
+    )
 
 from app.forms import LoginForm
 from . import auth
 
 
-@auth.route('/login')
+
+
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
-    
-    context:Dict = {
-        'login_form':login_form
+    context = {
+        'login_form': login_form
     }
-    
-    
-    return render_template('login.html',**context) 
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
+
+    return render_template('login.html', **context)
